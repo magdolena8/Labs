275d7c8dcad4b24f279a7ec5973ccc29613a0a0e
Ivan Begansky <ivanbegan16@gmail.com>
2021-10-06 21:15:52 +0300

    Lab_3 Done !

---
+++ Airline.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    /*partial*/public class Airline
+    {
+        public readonly string id;  //readonly field
+        internal string airplaneModel {get;} = "Airbus A380"; //const field
+        internal uint airNumber { get; set; }
+        internal string destination { get; set; } = "unknown";
+        internal string type { get; set; } = "unknown";
+        internal string days { get; set; } = "unknown";
+        internal double time { get; set; } = double.PositiveInfinity;
+        public static uint totalCreated { get; set; } = 0;
+
+
+        //Methods
+        public static void seeClassInfo() //Displays class info
+        {
+            Console.WriteLine("___Класс Airline управляет объектами(самолетами)___");
+        }
+
+        static void addPlain()
+        {
+            Airline.totalCreated++;
+        }
+        public /*partial */void seeInfo()
+        {
+            Console.WriteLine($"ID:\t\t {id}");
+            Console.WriteLine($"Airplane model:\t {airplaneModel}");
+            Console.WriteLine($"Airplane number: {airNumber}");
+            Console.WriteLine($"Destination:\t {destination}");
+            Console.WriteLine($"Airplane type:\t {type}");
+            Console.WriteLine($"Departure days:\t {days}");
+            Console.WriteLine($"Departure time:\t {time}\n\n");
+        }
+
+
+        //Constructors
+        public Airline()
+        {
+            Console.WriteLine("__ВЫ НЕ УКАЗАЛИ ПАРАМЕТРЫ__");
+        }
+        public Airline(uint airNumber, string destination, string type, string days, double time)
+        {
+            id = Guid.NewGuid().ToString(); //static contructor
+            this.airNumber = airNumber;
+            this.destination = destination;
+            this.type = type;
+            this.days = days;
+            this.time = time;
+            addPlain();
+        }
+        public Airline(uint num, string dest)
+        {
+            id = Guid.NewGuid().ToString(); //static contructor
+            airNumber = num;
+            destination = dest;
+            type = "unknown";
+            days = "unknown";
+            time = double.NaN;
+            addPlain();
+        }
+        public Airline(uint num)
+        {
+            id = Guid.NewGuid().ToString(); //static contructor
+            airNumber = num;
+            destination = "unknown";
+            type = "unknown";
+            days = "unknown";
+            time = double.NaN;
+            addPlain();
+        }
+
+        //Override
+        public override string ToString()
+        {
+            return ($"{id} {airNumber} {destination} {type} {days} {time}");
+
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+
+
+    //public partial class Info   //partial Class (display info)
+    //{
+    //    partial void seeInfo()
+    //    {
+    //        Console.WriteLine();
+    //    }
+    //}
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    //partial class Airline //partial Class (display info)
+    //{
+    //    public partial void seeInfo();
+    //}
+
+
+
+
+
+    class Program
+    {
+        public static void searchDestination(Airline[] flights)
+        {
+            Console.Clear();
+            Console.Write("Write airplane destination: ");
+            string city = Console.ReadLine().ToString();
+            foreach(Airline flight in flights)
+            {
+                if (flight.destination == city)
+                {
+                    flight.seeInfo();
+                    //return;
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static void searchDays(Airline[] flights)
+        {
+            Console.Clear();
+            Console.Write("Write race day: ");
+            string day = Console.ReadLine().ToString();
+            foreach (Airline flight in flights)
+            {
+                string[] subs = flight.days.Split(' ');
+                foreach (var str in subs)
+                {
+                    if(str == day)
+                    {
+                        flight.seeInfo();
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Airline[] flights = new Airline[5];
+
+            flights[0] = new Airline(144, "Paris", "Econom", "Mon Wed Fri", 12.15);
+            flights[1] = new Airline(228, "Kiev", "Econom", "Mon  Fri", 10.15);
+            flights[2] = new Airline(44, "Berlin", "Econom", "Mon Wed Fri", 6.45);
+            flights[3] = new Airline(653, "Paris", "Buisness", "Sun Mon", 10.45);
+            flights[4] = new Airline(813, "London", "Buisness", "Mon Wed Sat Sun", 22.30);
+            searchDestination(flights);
+            searchDays(flights);
+
+
+            Console.ReadKey();
+        }
+    }
+}
