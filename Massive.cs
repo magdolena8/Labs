53c0d7c72dfa5df6a6665ee2f865ad6de0933d31
magdolena8 <71251453+magdolena8@users.noreply.github.com>
2021-10-29 14:43:41 +0300

    Lab_4 Done!

---
+++ Massive.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Massive
+    {        
+        private int[] integers;
+        public int[] Integers   //Props
+        {  
+            get
+            {
+                return integers;
+            }
+            set
+            {
+                integers = value;
+            }
+        }
+
+        public Massive(params int[] n)  //Constructor
+        {
+            integers = n;
+        }
+
+        public int this [int index] //Indexator
+        {
+            get
+            {
+                return integers[index];
+            }
+        }
+
+        public static Massive operator +(Massive a, Massive b)  //Overloads
+        {
+            return new Massive {Integers = a.Integers.Concat(b.Integers).ToArray() };
+        }
+
+
+        public static Massive operator -(Massive a, Massive b)
+        {
+            Massive result = new Massive();
+            List<int> termsList = new List<int>();
+            foreach (var element in a.Integers)
+            {
+                if (b.Integers.Contains(element)){
+                    continue;
+                }
+                else
+                {
+                    termsList.Add(element);
+                }
+            }
+            result.Integers = termsList.ToArray();
+            return result;
+        }
+
+        public static bool operator >(int a, Massive mas)
+        {
+            if (mas.Integers.Contains(a))
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static bool operator <(int a, Massive mas)
+        {
+            throw new Exception("Error: unexpected operator");
+            
+        }
+
+        public static bool operator !=(Massive mas1, Massive mas2)
+        {
+            int i = 0;
+            foreach (var element in mas1.Integers)
+            {
+                if (element == mas2.Integers[i])
+                {
+                    i++;
+                    continue;
+                }
+                else return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(Massive mas1, Massive mas2)
+        {
+            int i = 0;
+            foreach (var element in mas1.Integers)
+            {
+                if (element == mas2.Integers[i])
+                {
+                    i++;
+                    continue;
+                }
+                else return false;
+            }
+            return true;
+        }
+
+        public static class Owner
+        {
+            public static readonly int id = 37137841;
+            public static readonly string name = "Ivan Begansky";
+            public static readonly string organization = "BSTU";
+
+            public static class Date
+            {
+                 public static DateTime creationDate = new DateTime(2021, 10, 09);
+            }
+        }
+    }
+    class StaticOperation
+    {
+        public static int Summ(Massive mass)
+        {
+            int counter = 0;
+            foreach (var i in mass.Integers)
+            {
+                counter += i;
+            }
+            return counter;
+        }
+
+        public static int MaxMinDifference(Massive mass) => 
+            mass.Integers.Max() - mass.Integers.Min();
+        public static int Lenght(Massive mass) => 
+            mass.Integers.Length;
+
+
+        public static class StringExtension
+        { 
+            public static string DeleteVowels(string str)
+            {
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (vowels.Contains(str[i]))
+                    {
+                        str = str.Remove(i, 1);
+                        i--;
+                    }
+                }
+                return str;
+            }
+
+            public static string DeleteFirstFiveLtrs(string str) =>
+                str.Remove(0, 5);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,66 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_3
-{
-    //partial class Airline //partial Class (display info)
-    //{
-    //    public partial void seeInfo();
-    //}
-
-
-
-
+namespace Lab_4
+{ 
 
     class Program
     {
-        public static void searchDestination(Airline[] flights)
+        public static void Display(Massive a)
         {
-            Console.Clear();
-            Console.Write("Write airplane destination: ");
-            string city = Console.ReadLine().ToString();
-            foreach(Airline flight in flights)
+            foreach (int i in a.Integers)
             {
-                if (flight.destination == city)
-                {
-                    flight.seeInfo();
-                    //return;
-                }
+                Console.Write(i+" ");
             }
-            Console.ReadKey();
-        }
-
-        public static void searchDays(Airline[] flights)
-        {
-            Console.Clear();
-            Console.Write("Write race day: ");
-            string day = Console.ReadLine().ToString();
-            foreach (Airline flight in flights)
-            {
-                string[] subs = flight.days.Split(' ');
-                foreach (var str in subs)
-                {
-                    if(str == day)
-                    {
-                        flight.seeInfo();
-                    }
-                }
-            }
-            Console.ReadKey();
+            Console.WriteLine();
         }
 
 
         static void Main(string[] args)
         {
-            Airline[] flights = new Airline[5];
+            Massive mas = new Massive(1, 12, 4, 5, 3);
+            Massive mas2 = new Massive(1, 12, 4, 5, 8);
+            Display(mas - mas2);
 
-            flights[0] = new Airline(144, "Paris", "Econom", "Mon Wed Fri", 12.15);
-            flights[1] = new Airline(228, "Kiev", "Econom", "Mon  Fri", 10.15);
-            flights[2] = new Airline(44, "Berlin", "Econom", "Mon Wed Fri", 6.45);
-            flights[3] = new Airline(653, "Paris", "Buisness", "Sun Mon", 10.45);
-            flights[4] = new Airline(813, "London", "Buisness", "Mon Wed Sat Sun", 22.30);
-            searchDestination(flights);
-            searchDays(flights);
+            Console.WriteLine(mas != mas2);
+            Console.WriteLine(StaticOperation.MaxMinDifference(mas));
+
+
+            Console.WriteLine(StaticOperation.StringExtension.DeleteFirstFiveLtrs("onetwothree"));
+
 
 
             Console.ReadKey();
